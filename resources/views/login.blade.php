c38ff3458157d3f2781e7efe5bcbdc54e51b634a
batakrisztian <batakrisztian97@gmail.com>
2022-10-26 13:18:20 +0200

    login action

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\DB;
 <div class="row">
 <div class="col-md-4"></div>
 <div class="col-md-4" >
-<form>
+<form method="get" class="form-validation" action="admin">
     <div>
         <label for="" class="form-validation mt-3">Felhasználónév:</label>
         <input type="text" name="username" id="username"  minlength="3"
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\DB;
     </div>
     <div>
         <label for="" class="form-validation mt-3">Jelszó:</label>
-        <input type="text" name="username" id="username"  minlength="3" class="form-control"
+        <input type="password" name="password" id="password"  minlength="3" class="form-control"
                             placeholder="Pl.: password">
     </div>
     <div align="center">
