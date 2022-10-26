01156b36bfb477238b8c3861118fcc935ba7e5e9
Pony2001 <csikoszs21@gmail.com>
2022-10-26 18:37:51 +0200

    ez ey cli commit

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -22,7 +22,7 @@
                         <img src="/images/logo.png" alt="" width="75" class="logo-img"> SzakiMaki
                     </a>
                 </div>
-                <div ><a href = "login"><button type="button" class="btn btn-secondary">Belépés</button></a></div>
+                <div class="login"><a href = "login"><button type="button" class="btn btn-secondary">Belépés</button></a></div>
                 
                
             </nav>
