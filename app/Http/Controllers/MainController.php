b6366ada5544ea7807e97f61c5a658cd8ba85939
Pony2001 <csikoszs21@gmail.com>
2022-10-27 12:27:06 +0200

    maincontroller: új nézet

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -73,6 +73,39 @@ class MainController extends Controller
     }
 
 
+    public function getSelects(){
+
+        $link = mysqli_connect('localhost', 'root', '', 'szakimaki');
+
+
+        $sql = "SELECT  employees.id, cities.county_id,counties.county,cities.id AS city_id,cities.city,employees.city_id,employees.name,field__employees.field_id,
+        fields.field, employees.phone, employees.email, employees.description
+        FROM employees
+        JOIN cities
+        ON employees.city_id=cities.id
+        JOIN counties
+        ON cities.county_id=counties.id
+        LEFT JOIN field__employees
+        ON employees.id=field__employees.employee_id
+        LEFT JOIN fields
+        ON field__employees.field_id=fields.id
+        GROUP BY employees.id;";
+
+        $return = [];
+        if ($result = mysqli_query($link, $sql)) {
+            if (mysqli_num_rows($result) > 0) {
+
+                while ($row = mysqli_fetch_array($result)) {
+                    $return[] = $row;
+                }
+            }
+        }
+
+        return $return;
+
+    }
+
+
 
     public function main()
     {
@@ -81,7 +114,9 @@ class MainController extends Controller
             [
                 //'fields' => $this->getFields(),
                 'cities' => $this->getCities(),
-                'counties' => $this->getCounties()
+                'counties' => $this->getCounties(),
+                'selects' => $this->getSelects()
+                
             ]
         );
     }
