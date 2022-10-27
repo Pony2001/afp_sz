733f6f0c30258db353a605d5a146fda41d22275a
batakrisztian <batakrisztian97@gmail.com>
2022-10-27 16:24:33 +0200

    breeze install

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,10 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 class LoginController extends Controller
 {
     public function login (){
         return view ('login');
+
     }
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'username' => ['required', 'username'],
+            'password' => ['required'],
+        ]);
+ 
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+ 
+            return redirect()->intended('dashboard');
+        }
+ 
+        return back()->withErrors([
+            'username' => 'Nincs ilyen felhasználónév!',
+        ])->onlyInput('username');
+}
 }
