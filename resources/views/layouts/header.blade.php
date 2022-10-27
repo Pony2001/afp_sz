4d47b93048f1055f9f004f7b3e60522d53491731
batakrisztian <batakrisztian97@gmail.com>
2022-10-27 14:47:16 +0200

    heADER

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,10 @@
+<nav class="navbar">
+    <div class="logo">
+        <a id="home" href="/" style="text-decoration: none; color: #ffcc00;">
+            <img src="/images/logo.png" alt="" width="75" class="logo-img"> SzakiMaki
+        </a>
+    </div>
+    <div class="login"><a href = "login"><button type="button" class="btn btn-secondary">Belépés</button></a></div>
+    
+   
+</nav>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -16,38 +16,20 @@
 <body>
     <div class='container'>
         <header>
-            <nav class="navbar">
-                <div class="logo">
-                    <a id="home" href="/" style="text-decoration: none; color: #ffcc00;">
-                        <img src="/images/logo.png" alt="" width="75" class="logo-img"> SzakiMaki
-                    </a>
-                </div>
-                <div class="login"><a href = "login"><button type="button" class="btn btn-secondary">Belépés</button></a></div>
-                
-               
-            </nav>
+            @include('layouts.header')
         </header>
         <content>
-
             @yield('content')
-
         </content>
         <footer class="py-3 my-4">
-            <hr>
-            <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-                <li class="nav-item nav-link px-2 text-muted">Bata Krisztián</li>
-                <li class="nav-item nav-link px-2 text-muted">Tápi Dániel</li>
-                <li class="nav-item nav-link px-2 text-muted">Csikós Zsolt Csaba</li>
-            </ul>
-            <p class="text-center text-muted"><a href="#" class="nav-link px-2 text-muted">Discord          Github</a></p>
-           
+            @include('layouts.footer')
         </footer>
     </div>
 </body>
 
 </html>
 
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
 </script>
+
