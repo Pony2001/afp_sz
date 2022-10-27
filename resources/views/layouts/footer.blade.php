b85b2c2b98e4e43dfb508974436c422f31a232c9
batakrisztian <batakrisztian97@gmail.com>
2022-10-27 14:47:04 +0200

    header_footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,9 @@
+ <hr>
+<ul class="nav justify-content-center border-bottom pb-3 mb-3">
+    <li class="nav-item nav-link px-2 text-muted">Bata Krisztián</li>
+    <li class="nav-item nav-link px-2 text-muted">Tápi Dániel</li>
+    <li class="nav-item nav-link px-2 text-muted">Csikós Zsolt Csaba</li>
+</ul>
+<p class="text-center text-muted"><a href="#" class="nav-link px-2 text-muted">Discord          Github</a></p>   
+
+
