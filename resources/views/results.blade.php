fc658526ae62f4441ca7ef85c726dc71e178f872
Pony2001 <csikoszs21@gmail.com>
2022-11-14 23:40:54 +0100

    show "not found" whenever $results is empty

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -10,85 +10,94 @@ use Illuminate\Support\Facades\DB;
 @extends('layouts.main')
 
 @section('content')
-    <h2>Eredmények</h2>
+    <br><br>
 
 
     <div class="row">
-        @foreach ($results as $employee)
+        @if ($results->isEmpty() == true)
             <div class="col-md-2">
             </div>
-            <div class="col-md-8 shadow bg-white rounded-5 listss">
-                <form action="/profile/{{ $employee->id }}" class="profile">
-                    <style>
-                        table,
-                        tr,
-                        td,
-                        th {
-                            border: solid 0px;
-                        }
-                    </style>
-                    <table>
-                        <tr>
-                            <td rowspan="4">
-                                <img src="https://picsum.photos/id/{{ $employee->id }}/200"
-                                    onerror="this.onerror=null; this.src='/images/unknown.png'" alt="" width="125"
-                                    class="shadow bg-white profile-picture" />
-                            </td>
-                        </tr>
-                        <div class="shadow bg-white rounded-5 profile">
+            <div class="col-md-8 alert alert-danger">
+                <h1 align="center">Nincs találat.</h1>
+                <h5 align="center"><a href="{{ url()->previous() }}" class="btn btn-danger">Vissza a szűrükhöz</a></h5>
+            </div>
+        @else
+            @foreach ($results as $employee)
+                <div class="col-md-2">
+                </div>
+                <div class="col-md-8 shadow bg-white rounded-5 listss">
+                    <form action="/profile/{{ $employee->id }}" class="profile">
+                        <style>
+                            table,
+                            tr,
+                            td,
+                            th {
+                                border: solid 0px;
+                            }
+                        </style>
+                        <table>
                             <tr>
-                                <th class="results-nev mt-2">
-                                    <h3>{{ $employee->name }}</h3>
-                                </th>
-
-                                <td class="results-szakma mt-3">
-
-                                    @foreach (\App\Models\City::where('id', '=', '' . $employee->city_id)->get() as $cities)
-                                        {{ $cities->city }}
-                                    @endforeach
-
-                                </td>
-                                <td rowspan="4"><button class="btn btn-warning shadow rounded-5"
-                                        type="submit">Megtekint</button>
+                                <td rowspan="4">
+                                    <img src="https://picsum.photos/id/{{ $employee->id }}/200"
+                                        onerror="this.onerror=null; this.src='/images/unknown.png'" alt=""
+                                        width="125" class="shadow bg-white profile-picture" />
                                 </td>
                             </tr>
-                            <tr>
-                                <td class="ps-2">
-                                    @foreach (\App\Models\Field_Employee::where('employee_id', '=', '' . $employee->id)->distinct()->get() as $fieldsId)
-                                        @foreach (\App\Models\Field::where('id', '=', '' . $fieldsId->field_id)->get() as $fields)
-                                            <span class="field shadow bg-white">{{ $fields->field }}</span>
+                            <div class="shadow bg-white rounded-5 profile">
+                                <tr>
+                                    <th class="results-nev mt-2">
+                                        <h3>{{ $employee->name }}</h3>
+                                    </th>
+
+                                    <td class="results-szakma mt-3">
+
+                                        @foreach (\App\Models\City::where('id', '=', '' . $employee->city_id)->get() as $cities)
+                                            {{ $cities->city }}
                                         @endforeach
-                                    @endforeach
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="results-overflow ms-2">
-                                    <p class="mt-3 description">{{ $employee->description }}</p>
-                                </td>
-                            </tr>
-                        </div>
-                    </table>
-                </form>
+
+                                    </td>
+                                    <td rowspan="4"><button class="btn btn-warning shadow rounded-5"
+                                            type="submit">Megtekint</button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="ps-2">
+                                        @foreach (\App\Models\Field_Employee::where('employee_id', '=', '' . $employee->id)->distinct()->get() as $fieldsId)
+                                            @foreach (\App\Models\Field::where('id', '=', '' . $fieldsId->field_id)->get() as $fields)
+                                                <span class="field shadow bg-white">{{ $fields->field }}</span>
+                                            @endforeach
+                                        @endforeach
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="results-overflow ms-2">
+                                        <p class="mt-3 description">{{ $employee->description }}</p>
+                                    </td>
+                                </tr>
+                            </div>
+                        </table>
+                    </form>
+                </div>
+
+                <div class="col-md-2">
+                </div>
+                <div class="col-md-12">
+                    <br><br>
+                </div>
+            @endforeach
+            <div class="d-flex">
+                <div class="mx-auto">
+                    {{ $results->links('layouts.paginationlinks') }}
+                </div>
             </div>
 
-            <div class="col-md-2">
-            </div>
-            <div class="col-md-12">
-                <br><br>
-            </div>
-        @endforeach
-        <div class="d-flex">
-            <div class="mx-auto">
-                {{ $results->links('layouts.paginationlinks') }}
-            </div>
-        </div>
 
-
-        <style>
-            .w-5 {
-                display: none;
-                /* eltünteti a nagy nyilakat a lap aljáról amik bugosan jelennek meg */
-            }
-        </style>
+            <style>
+                .w-5 {
+                    display: none;
+                    /* eltünteti a nagy nyilakat a lap aljáról amik bugosan jelennek meg */
+                }
+            </style>
+        @endif
     </div>
 @endsection
